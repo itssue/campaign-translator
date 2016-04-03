68d9bafcbd75734362486769faef2f59bba1b82e
itssue <its.sue@gmail.com>
2016-04-02 23:21:54 -0400

    campaign spending total on screen

--- js/app.js
+++ js/app.js
@@ -3,30 +3,11 @@ $(function(){
 	var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
 
 	$.get("http://realtime.influenceexplorer.com/api//new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey, function(resp){
+		var totalSpent = 0;
 		resp.results.forEach(function(child){
-			$('#container').append("<p>" + child.tot_spent + "</p>")
-		})			
+			totalSpent += Number(child.tot_spent);
+		})
+		$('#container').append("<p>" + totalSpent + "</p>");		
 	})
 
 })
-
-
-// var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
-
-// function campaignFn() {
-// 	var apiURL = "http://realtime.influenceexplorer.com/api/new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey;
-// 		$.get(apiURL, function(r) {
-// 			var data = r.results;
-
-// 			data.forEach(function() {
-// 				var spent = tot_spent;
-// 				$('#funds').text(spent);
-// 			});
-		
-
-// 		});
-// }
-
-// campaignFn();
-
-
