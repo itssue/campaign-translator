bb0a781588029356a0fe5d8e642864661ac3184a
itssue <its.sue@gmail.com>
2016-04-02 22:52:14 -0400

    added tot_spent to screen

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
-var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
+$(function(){
 
-function campaignFn() {
-	var apiURL = "http://realtime.influenceexplorer.com/api/new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey;
-		$.get(apiURL, function(r) {		
-			$('#funds').text(r.results);
+	var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
 
-		});
-}
+	$.get("http://realtime.influenceexplorer.com/api//new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey, function(resp){
+		resp.results.forEach(function(child){
+			$('#container').append("<p>" + child.tot_spent + "</p>")
+		})			
+	})
 
-campaignFn();
+})
 
 
-// var Rate = 4;
+// var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
 
-// function convertBTC(){
-// 	var fundsValue = document.getElementById("funds").value;
-// 	document.getElementById("item").value = convertToItem(fundsValue)
+// function campaignFn() {
+// 	var apiURL = "http://realtime.influenceexplorer.com/api/new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey;
+// 		$.get(apiURL, function(r) {
+// 			var data = r.results;
+
+// 			data.forEach(function() {
+// 				var spent = tot_spent;
+// 				$('#funds').text(spent);
+// 			});
+		
+
+// 		});
 // }
 
-// function convertToItem(dollarAmt) {
-// 	return dollarAmt/Rate;
-// }
+// campaignFn();
+
+
