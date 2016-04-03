f29427b1843d16fb9e592d09cb35e12f78caf919
itssue <its.sue@gmail.com>
2016-04-02 23:33:58 -0400

    change jQuery display for total

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ $(function(){
 
 	var apiKey = "81566c0b5b9d44dab813f065c2af1baf"
 
+	// Campaign Spending total on Screen
 	$.get("http://realtime.influenceexplorer.com/api//new_filing/?format=json&page=1&page_size=100&apikey=" + apiKey, function(resp){
 		var totalSpent = 0;
 		resp.results.forEach(function(child){
 			totalSpent += Number(child.tot_spent);
 		})
-		$('#container').append("<p>" + totalSpent + "</p>");		
+		$('#spending-total').text("$" + totalSpent)
 	})
 
 })
